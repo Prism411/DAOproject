1a4959bbdd4a2270532d0decbcb29023395eb3ee
Jáder Louis <jaderlouis3@gmail.com>
2023-01-17 21:48:45 -0300

    Renaming and Working on DAO

--- src/DAOexception/ExceptionError.java
+++
@@ -1,9 +0,0 @@
-package DAOexception;
-
-public class ExceptionError extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	public ExceptionError(String msg) {
-		super(msg);
-	}
-
-}
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,14 +1,14 @@
-package dao;
+package model.dao;
 
 import java.util.List;
-import entities.Department;
-import entities.Seller;
 
-public interface DepartmentDAO {
+import model.entities.Department;
+
+public interface DepartmentDao {
+
 	void insert(Department obj);
 	void update(Department obj);
 	void deleteById(Integer id);
 	Department findById(Integer id);
-	List<Department> findAll();	
-
+	List<Department> findAll();
 }
